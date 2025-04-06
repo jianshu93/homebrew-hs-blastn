7ab67dd0afa17939d13cb57d94fb6816ab1182d2
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2025-04-06 13:24:09 -0700

    add hs-blastn v2.0.0

---
+++ Formula/hs-blastn.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Hs-blastn < Formula
+  desc "a fast and accurate nucleotide-nucleotide sequences aligner"
+  homepage "https://github.com/jianshu93/HS-BLASTN"
+  url "https://github.com/jianshu93/HS-BLASTN/releases/download/v2.0.0/hs-blastn_Darwin_universal_v2.0.0.tar.gz"
+  sha256 "98fa3495e2f45f7bb9a3a4b8a9edb8781be0bf732305f2aeff4fe3ea4248f153"
+  version "2.0.0"
+
+  def install
+    bin.install "hs-blastn"
+  end
+end
